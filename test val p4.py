ee9a8a3a2506db4131e1054d0556e3051f47b12d
ymqnxi <v.m.yuen@hotmail.com>
2026-03-19 00:13:20 +0100

    Worked on cleaning artist_data, but was not able to find a good way for cleaning the dataset. Also changed the feature ranking function's name and added a parameter indicating whether the very low or very high scores ranking should be printed

--- test val p4.py
+++ test val p4.py
@@ -1,12 +1,34 @@
 import sqlite3
 import valerie_part4 as vp4
+import pandas as pd
 
 database = sqlite3.connect('spotify_database.db')
+# df = pd.read_sql_query("SELECT * FROM artist_data", database)
+# df['name'] = df['name'].str.title()
+# df.to_sql('artist_data', database, if_exists='replace', index=False)
 
-vp4.v(database)
-vp4.w(database)
-vp4.clean_artist_data(database)
+# vp4.w(database, 'Turbo')
+# print("\nArtist 1:")
+# vp4.v(database, '0TYydMAKPBYjZB0jgGCN7h')
+# print("\nArtist 2:")
+# vp4.v(database, '002HSjuWsGMinkXTa7JcRp')
+# # print("\nArtist 3:")
+# # vp4.v(database, '7n2Ycct7Beij7Dj7meI4X0')
+#
+# # print("")
+#
+# # print('\nDuplicate artists:')
+# # # vp4.clean_artist_data(database)
+# #
+# # # vp4.duplicates(database)
+# # vp4.duplicate_artists(database)
+#
+# # vp4.all_genres(database)
+#
+# # vp4.check_exact_duplicates(database)
+# vp4.check_duplicates(database)
 
-# vp4.most_frequent_genre_combinations(database)
+vp4.most_frequent_genre_combinations(database)
 
-# vp4.ranking_features(database, "danceability")
+vp4.top10_genres_feature_ranking(database, "danceability", very_low=True)
+vp4.top10_genres_feature_ranking(database, "danceability", very_low=False)
