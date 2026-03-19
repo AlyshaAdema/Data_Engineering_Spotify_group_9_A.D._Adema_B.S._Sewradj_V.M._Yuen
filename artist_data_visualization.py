8b604182af35985fe27ea142b8900ab9543cbdcc
AlyshaAdema <adema.a@outlook.com>
2026-03-19 12:57:10 +0100

    made the genre page of the dashboard and started on making the feature page

--- artist_data_visualization.py
+++ artist_data_visualization.py
@@ -2,6 +2,7 @@ import ast
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 import artist_data_analysis as arda
 
 def top10_followers(database, eras):
@@ -15,22 +16,74 @@ def top10_followers(database, eras):
     fig, ax = plt.subplots()
     ax.bar(top10_followers['name'], top10_followers['followers'])
     ax.set_xticklabels(top10_followers['name'], rotation=90)
-    ax.set_xlabel('Names top 10 artists')
+    ax.set_xlabel('Names artists')
     ax.set_ylabel('Followers')
     ax.set_title('Amount of followers top 10 artists')
     plt.tight_layout()
     return fig
 
-def top10_popularity(database):
-    df = pd.read_sql_query("SELECT name, artist_popularity FROM artist_data", database)
+def top10_popularity(database, eras):
+    if not eras:
+        fig, ax = plt.subplots()
+        ax.set_title("No eras selected")
+        return fig
+    eras_str = ','.join([f"'{era}'" for era in eras])
+    df = pd.read_sql_query(f"SELECT ar.name, MAX(ar.artist_popularity) as artist_popularity FROM artist_data ar JOIN albums_data al ON ar.id = al.artist_id WHERE era IN ({eras_str}) GROUP BY ar.name", database)
     top10_popularity = df.nlargest(10, 'artist_popularity')
-    plt.bar(top10_popularity['name'], top10_popularity['artist_popularity'])
-    plt.xticks(rotation=90)
-    plt.xlabel('Names top 10 artists')
-    plt.ylabel('Popularity')
-    plt.title('Popularity top 10 artists')
+    fig, ax = plt.subplots()
+    ax.bar(top10_popularity['name'], top10_popularity['artist_popularity'])
+    ax.set_xticklabels(top10_popularity['name'], rotation=90)
+    ax.set_xlabel('Names artists')
+    ax.set_ylabel('Artist Popularity')
+    ax.set_title('Popularity top 10 artists')
     plt.tight_layout()
-    plt.show()
+    return fig
+
+def top10_followers_genre(database, genre):
+    df = arda.top10_followers_artists_genre(database, genre)
+    fig, ax = plt.subplots()
+    ax.bar(df['name'], df['followers'])
+    ax.set_xticklabels(df['name'], rotation=90)
+    ax.set_xlabel('Names top artists')
+    ax.set_ylabel('Followers')
+    ax.set_title(f'Amount of followers top artists {genre}')
+    plt.tight_layout()
+    return fig
+
+def top10_popularity_genre(database, genre):
+    df = arda.top10_popularity_artists_genre(database, genre)
+    fig, ax = plt.subplots()
+    ax.bar(df['name'], df['artist_popularity'])
+    ax.set_xticklabels(df['name'], rotation=90)
+    ax.set_xlabel('Names top artists')
+    ax.set_ylabel('Popularity')
+    ax.set_title(f'Popularity top artists {genre}')
+    plt.tight_layout()
+    return fig
+
+def followers_distribution_genres(database, genre):
+    df = pd.read_sql_query("SELECT followers, artist_genres FROM artist_data", database)
+    df['artist_genres'] = df['artist_genres'].apply(ast.literal_eval)
+    df = df[df['artist_genres'].apply(lambda x: genre in x)]
+    fig, ax = plt.subplots()
+    sns.histplot(data=np.log1p(df['followers']), kde=True, ax=ax)
+    ax.set_title(f"Followers Distribution for {genre}")
+    ax.set_xlabel("Log(Followers + 1)")
+    ax.set_ylabel("Number of Artists")
+    plt.tight_layout()
+    return fig
+
+def popularity_distribution_genres(database, genre):
+    df = pd.read_sql_query("SELECT artist_popularity, artist_genres FROM artist_data", database)
+    df['artist_genres'] = df['artist_genres'].apply(ast.literal_eval)
+    df = df[df['artist_genres'].apply(lambda x: genre in x)]
+    fig, ax = plt.subplots()
+    sns.histplot(data=df['artist_popularity'], kde=True, ax=ax)
+    ax.set_title(f"Popularity Distribution for {genre}")
+    ax.set_xlabel("Popularity")
+    ax.set_ylabel("Number of Artists")
+    plt.tight_layout()
+    return fig
 
 def linear_regression(database, eras):
     if not eras:
@@ -71,5 +124,16 @@ def top10_genres(database, eras):
     ax.set_xlabel('Genres')
     ax.set_ylabel('Followers')
     ax.set_title('Amount of followers top 10 genres')
+    plt.tight_layout()
+    return fig
+
+def bar_plot_top_genre_combination(database, genre):
+    df = arda.most_frequent_combination_genre(database, genre)
+    fig, ax = plt.subplots()
+    ax.bar(df['genre'], df['count'])
+    ax.set_xticklabels(df['genre'], rotation=90)
+    ax.set_xlabel('Genre')
+    ax.set_ylabel('Appears together')
+    ax.set_title(f'Genres {genre} appears with most')
     plt.tight_layout()
     return fig
--- create dashboard.py
+++ create dashboard.py
@@ -1,13 +1,14 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
 import sqlite3
+
+# importing own files
 import artist_data_analysis as arda
 import artist_data_visualization as ardv
 import album_data_analysis as alda
-import full_database_analysis as fda
-import full_database_visualization as fdv
+import full_database_analysis as flda
+import full_database_visualization as fldv
+import features_data_analysis as fda
+import features_data_visualization as fdv
 
 st.set_page_config(
     page_title="Opening page",
@@ -20,7 +21,7 @@ database = sqlite3.connect('spotify_database.db')
 page = st.sidebar.radio('Navigation', ['Opening page', 'Features', 'Genres', 'Artist', 'Album'])
 eras = ['1900s','1930s','1940s','1950s','1960s','1970s','1980s','1990s','2000s','2010s','2020s']
 features = ['danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms']
-genres = ['pop', 'hiphop', 'rap', 'rock']
+genres = arda.all_genres(database)
 
 if page == 'Opening page':
     st.title("Opening page")
@@ -28,24 +29,30 @@ if page == 'Opening page':
     total_artists = arda.unique_artists(database, selected_eras)
     total_albums = alda.unique_albums(database, selected_eras)
     total_tracks = alda.unique_tracks(database, selected_eras)
-    col1, col2, col3 = st.columns(3)
+    total_genres = arda.number_of_genres(database, selected_eras)
+    col1, col2, col3, col4 = st.columns(4)
     with col1:
         st.metric("Total Artists", f"{total_artists:,.0f}")
     with col2:
         st.metric("Total Albums", f"{total_albums:,.0f}")
     with col3:
         st.metric("Total Tracks", f"{total_tracks:,.0f}")
-
+    with col4:
+        st.metric("Total Genres", f"{total_genres:,.0f}")
     st.divider()
 
-    fig = fdv.line_chart_track_popularity(database, selected_eras)
+    fig = fldv.line_chart_track_popularity(database, selected_eras)
     st.pyplot(fig)
 
     st.divider()
 
     left, right = st.columns(2)
     with left:
-        fig = ardv.top10_followers(database, selected_eras)
+        chart_type = st.selectbox("Select metric to display for top artists:", ("Followers", "Popularity"))
+        if chart_type == "Followers":
+            fig = ardv.top10_followers(database, selected_eras)
+        else:
+            fig = ardv.top10_popularity(database, selected_eras)
         st.pyplot(fig)
     with right:
         fig = ardv.linear_regression(database, selected_eras)
@@ -55,19 +62,108 @@ if page == 'Opening page':
 
     left, right = st.columns(2)
     with left:
-        fig = fdv.pie_chart_tracks(database, selected_eras)
+        fig = fldv.pie_chart_tracks(database, selected_eras)
         st.pyplot(fig)
     with right:
-        fig = fdv.pie_chart_explicit_vs_nonexplicit(database, selected_eras)
+        fig = fldv.pie_chart_explicit_vs_nonexplicit(database, selected_eras)
         st.pyplot(fig)
 
 elif page =='Features':
     st.title("Features")
-    selected_feature = st.sidebar.selectbox('Select a Feature to Display:', features)
+    selected_feature = st.sidebar.selectbox('Select a main Feature to display:', features)
+    compare_feature_list = [feature for feature in features if feature != selected_feature]
+    selected_eras = st.sidebar.multiselect('Select Era(s) to Display:', eras, default=eras)
+    mean_feature = fda.feature_stats(database, selected_feature, selected_eras, 'mean')
+    min_feature = fda.feature_stats(database, selected_feature, selected_eras, 'min')
+    max_feature = fda.feature_stats(database, selected_feature, selected_eras, 'max')
+    std_feature = fda.feature_stats(database, selected_feature, selected_eras, 'std')
+    correlation_feature0 = fda.feature_correlation(database, selected_feature, compare_feature_list[0], selected_eras)
+    correlation_feature1 = fda.feature_correlation(database, selected_feature, compare_feature_list[1], selected_eras)
+    correlation_feature2 = fda.feature_correlation(database, selected_feature, compare_feature_list[2], selected_eras)
+    correlation_feature3 = fda.feature_correlation(database, selected_feature, compare_feature_list[3], selected_eras)
+    correlation_feature4 = fda.feature_correlation(database, selected_feature, compare_feature_list[4], selected_eras)
+    correlation_feature5 = fda.feature_correlation(database, selected_feature, compare_feature_list[5], selected_eras)
+    correlation_feature6 = fda.feature_correlation(database, selected_feature, compare_feature_list[6], selected_eras)
+    correlation_feature7 = fda.feature_correlation(database, selected_feature, compare_feature_list[7], selected_eras)
+    correlation_feature8 = fda.feature_correlation(database, selected_feature, compare_feature_list[8], selected_eras)
+    st.markdown(f'Displayed feature: {selected_feature}')
+
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        st.metric("Mean", f"{mean_feature:,.2f}")
+    with col2:
+        st.metric("Minimum", f"{min_feature:,.2f}")
+    with col3:
+        st.metric("Maximum", f"{max_feature:,.2f}")
+    with col4:
+        st.metric("Standard Deviation", f"{std_feature:,.2f}")
+
+    st.divider()
+
+    left, right = st.columns(2)
+    with left:
+        fig = fdv.line_chart_features_eras(database, selected_feature, selected_eras)
+        st.pyplot(fig)
+    with right:
+        fig = fdv.boxplot_feature(database, selected_feature, selected_eras)
+        st.pyplot(fig)
+
+    st.divider()
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.metric(f"Correlation {compare_feature_list[0]} ", f"{correlation_feature0:,.2f}")
+        st.metric(f"Correlation {compare_feature_list[1]} ", f"{correlation_feature1:,.2f}")
+    with col2:
+        st.metric(f"Correlation {compare_feature_list[2]} ", f"{correlation_feature2:,.2f}")
+        st.metric(f"Correlation {compare_feature_list[3]} ", f"{correlation_feature3:,.2f}")
+    with col3:
+        st.metric(f"Correlation {compare_feature_list[4]} ", f"{correlation_feature4:,.2f}")
+        st.metric(f"Correlation {compare_feature_list[5]} ", f"{correlation_feature5:,.2f}")
+    with col4:
+        st.metric(f"Correlation {compare_feature_list[6]} ", f"{correlation_feature6:,.2f}")
+        st.metric(f"Correlation {compare_feature_list[7]} ", f"{correlation_feature7:,.2f}")
+    with col5:
+        st.metric(f"Correlation {compare_feature_list[8]} ", f"{correlation_feature8:,.2f}")
 
 elif page == 'Genres':
     st.title('Genres')
     selected_genre = st.sidebar.selectbox('Select a Genre to Display:', genres)
+    total_artists = arda.artists_per_genre(database, selected_genre)
+    average_popularity = arda.average_popularity_per_genre(database, selected_genre)
+    total_followers = arda.total_followers_per_genre(database, selected_genre)
+    average_followers = arda.average_followers_per_genre(database, selected_genre)
+    st.markdown(f'Displayed Genre: {selected_genre}')
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        st.metric("Total Artists", f"{total_artists:,.0f}")
+    with col2:
+        st.metric("Average Popularity", f"{average_popularity:,.0f}")
+    with col3:
+        st.metric("Total Followers", f"{total_followers:,.0f}")
+    with col4:
+        st.metric("Average Followers", f"{average_followers:,.0f}")
+
+    st.divider()
+
+    chart_type = st.selectbox("Select metric to display for top artists:", ("Followers", "Popularity"))
+    left, right = st.columns(2)
+    with left:
+        if chart_type == "Followers":
+            fig = ardv.top10_followers_genre(database, selected_genre)
+        elif chart_type == "Popularity":
+            fig = ardv.top10_popularity_genre(database, selected_genre)
+        st.pyplot(fig)
+    with right:
+        if chart_type == "Followers":
+            fig = ardv.followers_distribution_genres(database, selected_genre)
+        elif chart_type == "Popularity":
+            fig = ardv.popularity_distribution_genres(database, selected_genre)
+        st.pyplot(fig)
+
+    st.divider()
+    fig = ardv.bar_plot_top_genre_combination(database, selected_genre)
+    st.pyplot(fig)
 
 elif page == 'Artist':
     st.title("Artists")
@@ -75,14 +171,13 @@ elif page == 'Artist':
     feature = st.sidebar.selectbox('Select a Feature to Display:', features)
     number_followers = arda.number_followers_artist(database, name)
     popularity = arda.popularity_artist(database, name)
-    number_of_albums = fda.number_albums_artist(database, name)
-    number_of_singles = fda.number_singles_artist(database, name)
-    number_of_tracks = fda.number_tracks_artist(database, name)
-    feature_mean = fda.artist_features(database, name, feature, 'mean')
-    feature_median = fda.artist_features(database, name, feature, 'median')
-    feature_max = fda.artist_features(database, name, feature, 'max')
-    feature_min = fda.artist_features(database, name, feature, 'min')
-    feature_std = fda.artist_features(database, name, feature, 'std')
+    number_of_albums = flda.number_albums_artist(database, name)
+    number_of_singles = flda.number_singles_artist(database, name)
+    number_of_tracks = flda.number_tracks_artist(database, name)
+    feature_mean = flda.artist_features(database, name, feature, 'mean')
+    feature_max = flda.artist_features(database, name, feature, 'max')
+    feature_min = flda.artist_features(database, name, feature, 'min')
+    feature_std = flda.artist_features(database, name, feature, 'std')
     st.markdown(f'Displayed Artist: {name}')
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
@@ -102,10 +197,10 @@ elif page == 'Artist':
 
     left, right = st.columns(2)
     with left:
-        fig = fdv.bar_plot_top_5_tracks_artist(database, name)
+        fig = fldv.bar_plot_top_5_tracks_artist(database, name)
         st.pyplot(fig)
     with right:
-        fig = fdv.bar_plot_top_5_albums(database, name)
+        fig = fldv.bar_plot_top_5_albums(database, name)
         st.pyplot(fig)
 
     st.divider()
@@ -125,7 +220,7 @@ elif page == 'Artist':
             st.metric("Maximum", f"{feature_max:.2f}")
 
     with right:
-        fig = fdv.box_plot_feature_artist(database, name, feature)
+        fig = fldv.box_plot_feature_artist(database, name, feature)
         st.pyplot(fig)
 
 elif page == 'Album':
